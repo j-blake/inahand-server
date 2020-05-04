94a25e3f77cc76b887dd5911bbdfeb185c997825
j-blake <j-blake@users.noreply.github.com>
2020-05-04 06:27:16 -0500

    Add mocha and sinon

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,13 @@
 module.exports = {
+  plugins: [
+    'mocha',
+  ],
+  extends: [
+    'plugin:mocha/recommended',
+  ],
   env: {
     node: true,
-    browser: true,
     es6: true,
-    jest: true,
+    mocha: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'plugin:jest/recommended',
-    'plugin:jest/style',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    'jest',
-  ]
 };
