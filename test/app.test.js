09f3ebb0f6a38f37b6deac975e6d334096208ad4
j-blake <j-blake@users.noreply.github.com>
2020-09-05 07:49:29 -0500

    Save linter changes - single to double quotes

--- test/app.test.js
+++ test/app.test.js
@@ -1,9 +1,9 @@
-const assert = require('chai').assert;
-require('dotenv').config();
-const app = require('../server/app');
+const assert = require("chai").assert;
+require("dotenv").config();
+const app = require("../server/app");
 
-suite('app', function() {
-  test('app is mounted', function() {
-    assert.equal(app.mountpath, '/');
+suite("app", function () {
+  test("app is mounted", function () {
+    assert.equal(app.mountpath, "/");
   });
 });
--- test/authentication.test.js
+++ test/authentication.test.js
@@ -1,40 +1,40 @@
-const assert = require('chai').assert;
-const sinon = require('sinon');
-const authentication = require('../server/middleware/authentication');
-const identityService = require('../server/service/identity');
-const express = require('express');
+const assert = require("chai").assert;
+const sinon = require("sinon");
+const authentication = require("../server/middleware/authentication");
+const identityService = require("../server/service/identity");
+const express = require("express");
 
-const req = Object.create(express.request); 
+const req = Object.create(express.request);
 const res = Object.create(express.response);
 const next = () => {};
 
-suite('middleware - authentication', function() {
-  setup(function() {
-    sinon.stub(res, 'append').returns(res);
-    sinon.stub(res, 'send').returns(res);
-    sinon.stub(identityService, 'findOneById');
+suite("middleware - authentication", function () {
+  setup(function () {
+    sinon.stub(res, "append").returns(res);
+    sinon.stub(res, "send").returns(res);
+    sinon.stub(identityService, "findOneById");
   });
 
-  teardown(function() {
+  teardown(function () {
     sinon.restore();
   });
-  
-  test('missing session object returns 401', function() {
+
+  test("missing session object returns 401", function () {
     authentication(req, res, next);
     assert.equal(res.statusCode, 401);
   });
 
-  test('null identity returns 401', async function () {
-    req.session = { identity : 'narf' };
+  test("null identity returns 401", async function () {
+    req.session = { identity: "narf" };
     identityService.findOneById.resolves(null);
     await authentication(req, res, next);
     assert.equal(res.statusCode, 401);
   });
 
-  test('valid session appends identity to request and calls next()', async function() {
+  test("valid session appends identity to request and calls next()", async function () {
     const next = sinon.spy();
-    identity = { 'mockIdentity': 42 };
-    req.session = { identity : 'narf' };
+    identity = { mockIdentity: 42 };
+    req.session = { identity: "narf" };
     identityService.findOneById.resolves(identity);
     await authentication(req, res, next);
     assert.equal(req.identity, identity);
