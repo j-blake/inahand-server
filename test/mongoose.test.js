31530e15550df22188e863d4f17979fe0dc97752
j-blake <j-blake@users.noreply.github.com>
2020-09-05 07:50:07 -0500

    Save linter changes

--- test/mongoose.test.js
+++ test/mongoose.test.js
@@ -1,27 +1,26 @@
-const assert = require('chai').assert;
-const sinon = require('sinon');
-const mongoose = require('mongoose');
-const connectMongoose = require('../server/mongoose');
+const assert = require("chai").assert;
+const sinon = require("sinon");
+const mongoose = require("mongoose");
+const connectMongoose = require("../server/mongoose");
 
-
-suite('mongoose', function() {
-  setup(function() {
-    sinon.stub(mongoose, 'connect');
-    sinon.stub(process, 'exit');
-    sinon.stub(console, 'error');
+suite("mongoose", function () {
+  setup(function () {
+    sinon.stub(mongoose, "connect");
+    sinon.stub(process, "exit");
+    sinon.stub(console, "error");
   });
-  teardown(function() {
+  teardown(function () {
     mongoose.connect.restore();
     process.exit.restore();
     console.error.restore();
   });
-  test('connection to mongoose returns connection', async function() {
+  test("connection to mongoose returns connection", async function () {
     mongoose.connect.resolves(true);
-    assert(await connectMongoose())
+    assert(await connectMongoose());
   });
-  test('application exits on mongoose connection failure', async function() {
+  test("application exits on mongoose connection failure", async function () {
     mongoose.connect.throws();
     await connectMongoose();
     assert(process.exit.called);
-  })
+  });
 });
