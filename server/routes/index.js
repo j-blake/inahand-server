d3140f0681cf4c90af4b7fe4649566b4fbefc2f8
j-blake <j-blake@users.noreply.github.com>
2020-11-23 06:27:13 -0600

    Refactor scripts for prettier styling

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,15 @@ const path = require('path');
 const express = require('express');
 const authMiddleware = require('../middleware/authentication');
 
-const filterRoutesInDirectory = dir => fs.readdirSync(dir)
-  .filter(file => file.toLowerCase().indexOf('.js') !== -1 && !file.startsWith('index'))
-  /* eslint-disable-next-line global-require, import/no-dynamic-require */
-  .map(file => require(path.join(dir, path.basename(file, '.js'))));
+const filterRoutesInDirectory = (dir) =>
+  fs
+    .readdirSync(dir)
+    .filter(
+      (file) =>
+        file.toLowerCase().indexOf('.js') !== -1 && !file.startsWith('index')
+    )
+    /* eslint-disable-next-line global-require, import/no-dynamic-require */
+    .map((file) => require(path.join(dir, path.basename(file, '.js'))));
 
 const router = express.Router();
 const publicDirectory = path.join(__dirname, './public');
--- server/service/password.js
+++ server/service/password.js
@@ -2,22 +2,17 @@ const bcrypt = require('bcryptjs');
 const PasswordError = require('../error/password');
 
 exports.createPasswordHash = async (plaintextPassword) => {
-  try {
-    if (!/^[\w!@#$%^&*()-+=]{20,40}$/.test(plaintextPassword)) {
-      throw new PasswordError(
-        'passwords must be 20 to 40 alphanumeric and !@#$%&*()_-+= characters',
-      );
-    }
-    return await bcrypt.hash(plaintextPassword, parseInt(process.env.BCRYPT_SALT_ROUNDS, 10));
-  } catch (e) {
-    throw e;
+  if (!/^[\w!@#$%^&*()-+=]{20,40}$/.test(plaintextPassword)) {
+    throw new PasswordError(
+      'passwords must be 20 to 40 alphanumeric and !@#$%&*()_-+= characters'
+    );
   }
+  return bcrypt.hash(
+    plaintextPassword,
+    parseInt(process.env.BCRYPT_SALT_ROUNDS, 10)
+  );
 };
 
 exports.authenticatePassword = async (password, hash) => {
-  try {
-    return await bcrypt.compare(password, hash);
-  } catch (e) {
-    throw e;
-  }
+  return bcrypt.compare(password, hash);
 };
