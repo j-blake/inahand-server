7278b90cba3306752a41c3931f26b5b1f6a0e156
j-blake <j-blake@users.noreply.github.com>
2020-12-05 09:04:23 -0600

    Remove unused require

--- test/mockApp.js
+++ test/mockApp.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const Identity = require('../server/model/identity');
 
 const app = express();
 app.use(bodyParser.json());
