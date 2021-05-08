cf4d10147b6a2ff962efb8b0f3c705ce0785b9c5
Blake McAskill <blake.mcaskill@gmail.com>
2021-05-08 13:53:00 -0500

    Convert to HTTP server for use with heroku (#15)
    Co-authored-by: Blake McAskill <j-blake@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -1,19 +1,10 @@
 // Get dependencies
 require('dotenv').config();
 const path = require('path');
-const https = require('https');
-const fs = require('fs');
+const http = require('http');
 const appInit = require('./app');
 
-// https://docs.microsoft.com/en-us/azure/application-gateway/self-signed-certificates
-const options = {
-  key: fs.readFileSync(path.join(__dirname, '../server.key'), 'utf8'),
-  cert: fs.readFileSync(path.join(__dirname, '../server.crt'), 'utf8'),
-  ca: fs.readFileSync(path.join(__dirname, '../rootCA.key'), 'utf8'),
-  requestCert: false,
-  rejectUnauthorized: false,
-};
 appInit().then((app) => {
-  const httpsServer = https.createServer(options, app);
-  httpsServer.listen(process.env.HTTPS_PORT);
+  const httpsServer = http.createServer(app);
+  httpsServer.listen(process.env.HTTP_PORT);
 });
